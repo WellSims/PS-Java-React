bcf3b34d7e0a60287fd8057ad74f251d8b5889da
Welington Simões <welin099@gmail.com>
2023-07-17 23:18:43 -0300

    Criação de metodo que retorna lista de transferências a partir do id de uma conta

--- src/main/java/br/com/banco/service/TransferenciaService.java
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -1,5 +1,26 @@
 package br.com.banco.service;
 
-public class TransferenciaService {
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.banco.model.Transferencia;
+import br.com.banco.repository.TransferenciaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class TransferenciaService {
+	
+	@Autowired
+	private TransferenciaRepository transferenciaRepository;
+	
+	@Transactional
+	public List<Transferencia> findByConta(Long idConta){
+		return transferenciaRepository.findByConta(idConta);
+	}
+	
 }
