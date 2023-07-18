006309c741d51e507fdbe949ba38c659e569dc28
Welington Simões <welin099@gmail.com>
2023-07-17 23:19:07 -0300

    Criação de método para retornar conta a partir do id

--- src/main/java/br/com/banco/service/ContaService.java
+++ src/main/java/br/com/banco/service/ContaService.java
@@ -1,8 +1,24 @@
 package br.com.banco.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.banco.model.Conta;
+import br.com.banco.repository.ContaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Service
 public class ContaService {
 
+	@Autowired
+	private ContaRepository contaRepository;
+	
+	@Transactional
+	public Conta findById(Long id){
+		return contaRepository.findById(id).get();
+	}
+	
 }
