0c52c33b125364ba0206215f3f3cb9f7de27a55a
welington.simoes <welington.simoes@tcm.ba.gov.br>
2023-07-18 17:18:40 -0300

    Criadas novas consultas para operador e range de datas

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -1,5 +1,6 @@
 package br.com.banco.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,15 @@ import br.com.banco.model.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM transferencia where transferencia.conta_id = :idConta")
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE transferencia.conta_id = :idConta")
 	public List<Transferencia> findByConta(Long idConta);
 	
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE transferencia.nome_operador_transacao = :operador")
+	public List<Transferencia> findByOperador(String operador);
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE data_transferencia BETWEEN :initial AND :end")
+    List<Transferencia> findByDateTime(LocalDateTime initial, LocalDateTime end);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE nome_operador_transacao = :operador AND data_transferencia BETWEEN :initial AND :end")
+    List<Transferencia> findByOperadorDateTime(String operador, LocalDateTime initial, LocalDateTime end);
 }
