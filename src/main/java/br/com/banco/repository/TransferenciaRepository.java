5b7d1400a179e4e69e9845ca59b7d064a54443c2
Welington Simões <welin099@gmail.com>
2023-07-17 23:17:45 -0300

    Criação de consulta para consultar transferências a partir do ida conta

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -1,8 +1,15 @@
 package br.com.banco.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import br.com.banco.model.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
-
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia where transferencia.conta_id = :idConta")
+	public List<Transferencia> findByConta(Long idConta);
+	
 }
