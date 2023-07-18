2b756936b581a8a4bda68614620ba1f31565b7f8
Welington Simões <welin099@gmail.com>
2023-07-17 23:20:11 -0300

    Criação de endpoint/subrecurso para retornar lista de transferências a partir do id de uma conta

--- src/main/java/br/com/banco/controller/ContaController.java
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -1,8 +1,41 @@
 package br.com.banco.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.banco.model.Conta;
+import br.com.banco.model.Transferencia;
+import br.com.banco.service.ContaService;
+import br.com.banco.service.TransferenciaService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RequestMapping("/conta")
+@RestController
 public class ContaController {
+
+	@Autowired
+	private ContaService contaService;
+	
+	@Autowired
+	private TransferenciaService transferenciaService;
+	
+	
+	@GetMapping("/{id}")
+	public Conta findById(@PathVariable Long id){
+		Conta t = contaService.findById(id);
+		return t;
+	}
+	
+	@GetMapping("/{idConta}/transferencias")
+	public List<Transferencia> findByConta(@PathVariable Long idConta){
+		List<Transferencia> t = transferenciaService.findByConta(idConta);
+		return t;
+	}
 
 }
