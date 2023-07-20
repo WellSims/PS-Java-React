76dcff8abca59e10ef1ee9c1ade0692c9247013e
Welington Simões <welin099@gmail.com>
2023-07-19 21:34:14 -0300

    Modificação nas consultas de TransferenciaRepository

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -10,15 +10,17 @@ import br.com.banco.model.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE transferencia.conta_id = :idConta")
+	public List<Transferencia> findAllByOrderByContaAsc();
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE transferencia.conta_id = :idConta order by transferencia.conta_id asc")
 	public List<Transferencia> findByConta(Long idConta);
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE transferencia.nome_operador_transacao = :operador")
 	public List<Transferencia> findByOperador(String operador);
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE data_transferencia BETWEEN :initial AND :end")
-    List<Transferencia> findByDateTime(LocalDateTime initial, LocalDateTime end);
+	@Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE data_transferencia BETWEEN :dataInicio AND :dataFim")
+	public List<Transferencia> findByDataTransferencia(LocalDateTime dataInicio, LocalDateTime dataFim);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE nome_operador_transacao = :operador AND data_transferencia BETWEEN :initial AND :end")
-    List<Transferencia> findByOperadorDateTime(String operador, LocalDateTime initial, LocalDateTime end);
+    @Query(nativeQuery = true, value = "SELECT * FROM transferencia WHERE nome_operador_transacao = :operador AND data_transferencia BETWEEN :dataInicio AND :dataFim")
+    public List<Transferencia> findByDataTransferenciaAndOperador(LocalDateTime dataInicio, LocalDateTime dataFim, String operador);
 }
